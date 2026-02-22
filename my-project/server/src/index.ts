9c4f5574502795f717ab92219a76b688baefac67
Techy CA <mr.dashsolo@gmail.com>
2026-02-23 01:42:46 +0530

    10000

--- my-project/server/src/index.ts
+++ my-project/server/src/index.ts
@@ -45,5 +45,5 @@ mongoose.connect(MONGO_URI, {
     process.exit(1);
   });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
